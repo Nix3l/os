a6bb93a1b44f90d81995a5426a19d7e46cbdc291
nix3l <momanianas123@gmail.com>
2025-03-18 16:27:50 +0300

    actually loading on qemu now

--- Makefile
+++ Makefile
@@ -1,21 +1,34 @@
 CC := i686-elf-gcc
+LD := i686-elf-ld
 ASM := i686-elf-as
 
 LD_SCRIPT := linker.ld
 
+CC_FLAGS := -nostdlib -ffreestanding -T ${LD_SCRIPT} -fno-exceptions -mno-red-zone -Wall -Wextra -Werror -m32
+
 QEMU := qemu-system-i386
 VM_FLAGS := -cpu pentium3
 
-all: run
+BUILD_DIR := build
 
-boot.o: bootloader/boot.asm
+all: run clean
+
+${BUILD_DIR}/boot.o: bootloader/boot.asm
 	${ASM} $^ -o $@
 
-out.bin: boot.o
-	${CC} ${CCFLAGS} $^ -o $@
+#${BUILD_DIR}/out.bin: ${BUILD_DIR}/boot.o
+#	${CC} ${CCFLAGS} $^ -o $@
 
-run: out.bin
-	${QEMU} ${VM_FLAGS} -hda $<
+${BUILD_DIR}/out.bin: ${BUILD_DIR}/boot.o
+	i686-elf-ld -T ${LD_SCRIPT} $^ -o $@
+
+${BUILD_DIR}/os.img: ${BUILD_DIR}/out.bin
+	dd if=/dev/zero of=$@ bs=512 count=2880
+	mkfs.fat -F 12 -n "VOLUME" $@
+	dd if=$< of=$@ conv=notrunc
+	
+run: ${BUILD_DIR}/os.img
+	${QEMU} ${VM_FLAGS} -fda $<
 
 clean:
-	rm *.o *.bin
+	rm -f ${BUILD_DIR}/*.o ${BUILD_DIR}/*.bin
--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -1,6 +1,7 @@
 .att_syntax
 .code16
 
+.section boot
 .text
 .org 0x0 # start all addresses at 0x0 TODO(nix3l): shouldnt this be 0x7c00?
 
@@ -12,10 +13,10 @@ boot_enter:
 # https://jdebp.uk/FGA/bios-parameter-block.html
 # https://averstak.tripod.com/fatdox/bootsec.htm [ <-- this one is the easiest do decipher ]
 # https://en.wikipedia.org/wiki/Design_of_the_FAT_file_system
-# in this case the FAT16 filesystem is used
+# in this case the FAT12 filesystem is used
 boot_parameter_block:
-    pOEM: 		        .ascii "os______" 	# OEM string, has to occupy 8 bytes
-    pSectSize: 		    .word 0x200 		# size of sector (in bytes)
+    pOEM: 		        .ascii "os      " 	# OEM string, has to occupy 8 bytes
+    pSectorSize: 		.word 0x512 		# size of sector (in bytes)
     pClusterSize: 	    .byte 1 		    # sectors per cluster (allocation unit)
     pReservedSectors: 	.word 1 		    # number of reserved sectors
     pFATCount: 		    .byte 2 		    # number of FATs
@@ -23,17 +24,17 @@ boot_parameter_block:
     pSectorCount: 	    .word 2880 		    # number of sectors in the volume
     pMedia: 		    .byte 0xF0 		    # media descriptor byte
     pFATSize: 		    .word 9 		    # size of each FAT
-    pSectorsPerFAT: 	.word 9 		    # sectors per track
+    pSectorsPerTack: 	.word 18 		    # sectors per track
     pHeadCount: 	    .word 2 		    # number of heads
     pHiddenSectors: 	.int  0 			# number of hidden sectors (in this case theres none)
-    pSectors32: 	    .int  0 			# number of sectors over 32Mb
+    pSectors32: 	    .int  0 			# number of sectors over 32Mb (large sectors)
     pBootDrive: 	    .byte 0 		    # holds the drive the boot sector came from
-    pReserved: 		    .byte 0 		    # not exactly sure, but it seems to be reserved?
+			 		    .byte 0 		    # reserved. not sure why. it just is.
     ### EBPB ###
     pBootSign: 		    .byte 0x29 		        # signal start of EBPB
-    pVolumeID: 		    .ascii "seri" 		    # disk serial
-    pVolumeLabel: 	    .ascii "volume     " 	# volume label
-    pFSType: 		    .ascii "FAT16   " 	    # file system type
+    pVolumeID: 		    .ascii "seri" 		    # volume serial number, doesnt really matter
+    pVolumeLabel: 	    .ascii "VOLUME     " 	# volume label
+    pFSType: 		    .ascii "FAT12   " 	    # file system type
 
 # NOTE(nix3l): essentially we have to:
 #   => reset the floppy disk system TODO(nix3l): ???
@@ -63,6 +64,8 @@ boot_start:
 
 	# TODO(nix3l): find out how to read the filesystem
 	# read the kernel from disk into memory
+halt:
+	jmp halt
 
 .org 510
 .word 0xaa55 # magic word
